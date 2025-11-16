2ab5766b229869a992d1416bbc1ec7b0ca3a3625
Sahil-Soni18 <sahils1810@gmail.com>
2025-11-16 14:38:36 +0530

    Label Extraction setup...

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
-print('Placeholder...')
+import pandas as pd
+from sklearn.preprocessing import MultiLabelBinarizer
+
+df = pd.read_csv('./data.csv')
+
+df['tone_vector'] = df['tone_vector'].apply(lambda x: x.split(','))
+
+mlb = MultiLabelBinarizer()
+Y = mlb.fit_transform(df['tone_vector'])
+print(f"Labels: {mlb.classes_}")
